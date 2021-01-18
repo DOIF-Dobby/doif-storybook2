3dc1637a3712330bb9c21a7915c9ae9cba902677
doif-dobby <doif.dobby@gmail.com>
2021-01-18 22:41:54 +0900

    [#2] 버튼 컴포넌트 export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 // 컴포넌트 export
-export { default as MyButton } from './MyButton';
+export { default as Button } from './components/buttons/Button';
 
 // 테마 export
 export { theme } from './styles/themes';
-export { DoifThemeProps } from './styles/themes/DoifThemeProps';
+export {
+  DoifThemeProps,
+  DoifThemeColorProps,
+  DoifThemeVariantProps,
+} from './styles/themes/DoifThemeProps';
 
 // 스타일 export
 export { GlobalStyle } from './styles/global-style';
