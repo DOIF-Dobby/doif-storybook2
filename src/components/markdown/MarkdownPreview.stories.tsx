94e79e5913305135971999f1d927c79b0800c9e3
doif-dobby <doif.dobby@gmail.com>
2021-02-08 22:50:50 +0900

    [#19] react-split-pane 추가

--- src/components/markdown/MarkdownPreview.stories.tsx
+++ src/components/markdown/MarkdownPreview.stories.tsx
@@ -5,6 +5,8 @@ import Container from '../container/Container';
 import Page from '../common/Page';
 import Box from '../common/Box';
 import Textarea from '../textarea/Textarea';
+import SplitPane, { Pane } from 'react-split-pane';
+import styled from 'styled-components';
 
 export default {
   title: 'Components/MarkdownPreview',
@@ -128,12 +130,80 @@ const Template: Story<ComponentProps<typeof MarkdownPreview>> = (args) => {
   }, []);
 
   return (
-    <Container>
-      <Textarea value={content} onChange={onChange} width="50%" rows={50} />
-      <MarkdownPreview {...args} content={content} />
-    </Container>
+    <StyleContainer>
+      <SplitPane
+        split="vertical"
+        defaultSize="50%"
+        style={{ position: 'relative' }}
+      >
+        <Textarea
+          value={content}
+          onChange={onChange}
+          style={{ height: '100%' }}
+        />
+        <MarkdownPreview {...args} content={content} />
+      </SplitPane>
+    </StyleContainer>
   );
 };
+
+const StyleContainer = styled.div`
+  .Pane {
+    & > div {
+      height: 100%;
+    }
+  }
+
+  .Resizer {
+    background: #000;
+    opacity: 0.2;
+    z-index: 1;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    -moz-background-clip: padding;
+    -webkit-background-clip: padding;
+    background-clip: padding-box;
+  }
+
+  .Resizer:hover {
+    -webkit-transition: all 2s ease;
+    transition: all 2s ease;
+  }
+
+  .Resizer.horizontal {
+    height: 11px;
+    margin: -5px 0;
+    border-top: 5px solid rgba(255, 255, 255, 0);
+    border-bottom: 5px solid rgba(255, 255, 255, 0);
+    cursor: row-resize;
+    width: 100%;
+  }
+
+  .Resizer.horizontal:hover {
+    border-top: 5px solid rgba(0, 0, 0, 0.5);
+    border-bottom: 5px solid rgba(0, 0, 0, 0.5);
+  }
+
+  .Resizer.vertical {
+    width: 11px;
+    margin: 0 -5px;
+    border-left: 5px solid rgba(255, 255, 255, 0);
+    border-right: 5px solid rgba(255, 255, 255, 0);
+    cursor: col-resize;
+  }
+
+  .Resizer.vertical:hover {
+    border-left: 5px solid rgba(0, 0, 0, 0.5);
+    border-right: 5px solid rgba(0, 0, 0, 0.5);
+  }
+  .Resizer.disabled {
+    cursor: not-allowed;
+  }
+  .Resizer.disabled:hover {
+    border-color: transparent;
+  }
+`;
 
 export const Default = Template.bind({});
 Default.args = {};
